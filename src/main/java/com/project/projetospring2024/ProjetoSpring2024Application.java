bb01c7f4c4f9a02188cdda36b9d965e4156292f0
Diego Silva <diegosilva157890@gmail.com>
2024-04-18 17:57:38 -0300

    Inicio do Projeto

---
+++ src/main/java/com/project/projetospring2024/ProjetoSpring2024Application.java
@@ -0,0 +1,13 @@
+package com.project.projetospring2024;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpring2024Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpring2024Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/project/projetospring2024/ProjetoSpring2024ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.projetospring2024;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetoSpring2024ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
